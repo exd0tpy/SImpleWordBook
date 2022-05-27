5fd26cad5cd0837f276888588a1f0259685153c0
exd0tpy <jsw5258@ajou.ac.kr>
2022-05-28 00:07:42 +0900

    test

--- app/src/main/java/com/appb34r/simplewordbook/MainActivity.kt
+++ app/src/main/java/com/appb34r/simplewordbook/MainActivity.kt
@@ -6,6 +6,7 @@ import com.appb34r.simplewordbook.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
 
+    //test
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
